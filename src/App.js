000ecf24f1f2f2ef09388f2395b935d6430016be
zoh <zoh@bk.ru>
2019-01-21 15:19:08 +0300

    update NumberInput to react 16

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-// import NumberInput        from 'components/NumberInput'
-
+import NumberInput from "components/NumberInput";
 
 class App extends Component {
+  state = {
+    number: null
+  };
+
+  onChange = val => {
+    this.setState({ number: val });
+  };
+
   render() {
     return (
       <div className="App">
         <p className="App-intro">
           Comma Number input
+          <NumberInput value={this.state.number} onChange={this.onChange} />
         </p>
       </div>
     );
